780c97e7d879159a16ccb755c232d1b99b8f0bae
KelvinZhong <kelvinzhong.22@ichat.sp.edu.sg>
2023-04-27 11:48:40 +0800

    yeyyy 1.2

--- Lab2.py
+++ Lab2.py
@@ -30,21 +30,32 @@ def find_min_max(list):
        if num > max_val:
            max_val = num
    return min_val, max_val
-def sort_temperature():
+def sort_temperature(list):
     print("sort_temperature")
-
-def calc_median_temperature():
+    sorted_list = sorted(list)
+    return sorted_list
+def calc_median_temperature(list):
  print("calc_median_temperature")
-
+ sorted_list = sorted(list)
+ n = len(sorted_list)
+ if n % 2 == 0:
+     mid1 = sorted_list[n // 2 - 1]
+     mid2 = sorted_list[n // 2]
+     median = (mid1 + mid2) / 2
+ else:
+     median = sorted_list[n // 2]
+ return median
 
 display_main_menu()
 temp_list = get_user_input()
 average = calc_average(temp_list)
 min_val, max_val = find_min_max(temp_list)
-
+sorted_list = sort_temperature(temp_list)
+median = calc_median_temperature(temp_list)
 
 print(temp_list)
 print("The average temp is " + str(average))
 print("The minimum temperature is:", min_val)
 print("The maximum temperature is:", max_val)
-
+print("The sorted Temperature in order is:", sorted_list)
+print("The median Temperature is:", median)
