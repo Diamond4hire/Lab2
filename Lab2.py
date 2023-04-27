177bec17457d1c3cccfb14ac3a69bc90b028e914
KelvinZhong <kelvinzhong.22@ichat.sp.edu.sg>
2023-04-27 11:49:41 +0800

    'yey

--- Lab2.py
+++ Lab2.py
@@ -3,7 +3,7 @@ def display_main_menu():
  print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 def get_user_input():
   print("get_user_input")
-  values = input("Enter a list of strings separated by commas: ")
+  values = input("Enter a list of Temperatures separated by commas: ")
   temperatures = values.split(",")
   temp_list = [float(x) for x in temperatures]
   return temp_list
