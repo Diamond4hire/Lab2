c5722b15e42835636297d5ce5c777992cf656ca1
KelvinZhong <kelvinzhong.22@ichat.sp.edu.sg>
2023-05-04 04:06:57 +0800

    yey

--- bmi.py
+++ bmi.py
@@ -1,3 +1,7 @@
+def main():
+    print (calculate_bmi(weight=57, height=1.73))
+
+
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
@@ -8,9 +12,13 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
      print("The User is Under Weight")
+     return -1
     elif bmi > 25.0:
      print("The User is Over Weight")
+     return 1
     else:
      print("The User is Normal Weight")
+     return 0
 
-calculate_bmi(weight=57, height=1.73)
+if __name__ == "__main__":
+ main()
